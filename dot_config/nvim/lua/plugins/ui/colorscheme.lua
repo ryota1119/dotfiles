7408a7df39078a41ea5004dd9fa52f256c775633
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2026-06-10 18:28:56 +0900

    fix(nvim): リモートmainとのコンフリクトを解消

--- dot_config/nvim/lua/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/plugins/ui/colorscheme.lua
@@ -5,7 +5,6 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = false,
       integrations = {
         blink_cmp = true,
         bufferline = true,
